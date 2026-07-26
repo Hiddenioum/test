3f78818069a20b9e5baa66b75bf51e3047d77618
Hiddenioum <Hiddenioum@gmail.com>
2026-07-26 16:23:59 +0330

    ci: update workflow to copy patched_files directly

---
+++ patched_files/Telegram/SourceFiles/api/api_send_progress.cpp
@@ -0,0 +1,7 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please see LEGAL file in the code repository.
+*/
+#include "api/api_send_progress.cpp"
